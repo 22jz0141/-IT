41289e3aa4c9ac5d649371f62806226ea5be0c26
22jz0141 <22jz0141@jec.ac.jp>
2024-10-08 10:23:55 +0900

    学習写真プログラム

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 # カメラを開く
 cap = cv2.VideoCapture(0)
@@ -7,17 +8,27 @@ indexNo = 0
 while True:
     # 画像をキャプチャする
     ret, frame = cap.read()
-
+    
     # 画像を表示する
+    cv2.imwrite(f"cameraPhoto\image{indexNo:05d}.jpg", frame)
+    print(f"image{indexNo:05d}.jpg")
+    indexNo = indexNo + 1
+    time.sleep(0.5)
+
+    dst_img = cv2.rectangle( img = frame,
+    pt1 = (200,100),
+    pt2 = (450,350),
+    color = (0,255,0),
+    thickness = 3,
+    lineType = 4 )
     cv2.imshow("Image", frame)
+
+
 
     # `q`キーを押すとループを終了する
     if cv2.waitKey(1) == ord('q'):
         break
-    elif cv2.waitKey(1) == ord('c'):
-        cv2.imwrite("C:\Users\Harut\advanced-IT\img\image{indexNo:05d}.jpg", frame)
-        print(f"image{indexNo:05d}.jpg")
-        indexNo = indexNo + 1
+    # elif cv2.waitKey(1) == ord('c'):
 # カメラを閉じる
 cap.release()
 # すべてのウィンドウを閉じる
